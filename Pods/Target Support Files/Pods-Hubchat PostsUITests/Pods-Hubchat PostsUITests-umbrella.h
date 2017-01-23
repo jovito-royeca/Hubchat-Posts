6829fece7318b76e6e8f3b757e113c95f5f35d24
Jovito Royeca <jovit.royeca@gmail.com>
2017-01-23 16:57:58 +0800

    feat: Added Cocoapods

---
+++ Pods/Target Support Files/Pods-Hubchat PostsUITests/Pods-Hubchat PostsUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Hubchat_PostsUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hubchat_PostsUITestsVersionString[];
+

ba7604a88a9be390d894ea46abd8c2df42b5a7f8
Jovito Royeca <jovit.royeca@gmail.com>
2017-01-23 16:31:02 +0800

    feat: Created Xcode project

---
+++ Hubchat Posts/Hubchat PostsTests/Hubchat_PostsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hubchat_PostsTests.swift
+//  Hubchat PostsTests
+//
+//  Created by Jovito Royeca on 23/01/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import XCTest
+@testable import Hubchat_Posts
+
+class Hubchat_PostsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
